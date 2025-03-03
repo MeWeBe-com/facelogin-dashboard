185d2677b49532e24e57a480ac91e83a53a33100
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2025-03-03 17:01:20 +0500

    routing issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
-
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import { redirect } from "next/navigation";
 import { useCookies } from 'next-client-cookies';
 
 export default function Home() {
   const cookies = useCookies();
   const searchParams = useSearchParams();
-  const id = searchParams.get('org_id'); 
-    console.log(id)
-  if (id) {
-    cookies.set('org_id', id)
-    redirect("/main/dashboard"); // Redirects users to the login page
-  } else {
-    redirect("/auth/login"); // Redirects users to the login page
-  }
 
-  return null; // This prevents any content from rendering
+  useEffect(() => {
+    const id = searchParams.get('org_id');
+    if (id) {
+      cookies.set('org_id', id);
+      redirect("/main/dashboard");
+    } else {
+      redirect("/auth/login");
+    }
+  }, [searchParams]); // Runs whenever search params change
+
+  return null; // Prevents rendering any content
 }
